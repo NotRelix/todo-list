9b33c186faa26cfbc469f5569a266e2fecbf280a
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-03-09 21:15:42 +0800

    feat: load and populate data from data.json

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ class Project {
 
   #tasks = [];
 
+  get projectName() { return this.name; }
+  get tasks() { return this.#tasks.slice(); }
+
   addTask(task) {
     this.#tasks.push(task);
   }
@@ -36,3 +39,35 @@ class Task {
     console.log(`${this.title} ${this.desc} ${this.dueDate} ${this.priority}`);
   }
 }
+
+function populateProjects(projects, projectName) {
+  const newProject = new Project(projectName);
+  projects.push(newProject);
+}
+
+function populateTasks(projects, task, projectId) {
+  const newTask = new Task(task.title, task.desc, task.dueDate, task.priority);
+  const projectIndex = projectId - 1;
+  projects[projectIndex].addTask(newTask);
+}
+
+function populateInitialData(projects) {
+  for (let projectName in data) {
+    const projectId = data[projectName].project_id;
+    populateProjects(projects, projectName);
+    for (let task of data[projectName].tasks) {
+      populateTasks(projects, task, projectId);
+    }
+  }
+}
+
+const projects = [];
+
+populateInitialData(projects);
+
+// Check if its populated from data.json
+console.log(projects[0].projectName);
+projects[0].displayAllTasks();
+
+console.log(projects[1].projectName);
+projects[1].displayAllTasks();
