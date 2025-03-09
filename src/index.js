d1f67d64242fd1b2eb5fdde3337d53f394d29be5
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-03-09 10:58:54 +0800

    feat: add Task class with display method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './style.css';
 
-console.log('test');
+class Task {
+  constructor(title, desc, dueDate, priority) {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+
+  displayTask() {
+    console.log(`${this.title} ${this.desc} ${this.dueDate} ${this.priority}`);
+  }
+}
+
+const task1 = new Task('shower', 'first desc', 'today', 'high');
+const task2 = new Task('code', 'second desc', 'tomorrow', 'medium');
+
+task1.displayTask();
+task2.displayTask();
