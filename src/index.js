cc55101a8420bb571b11cde9cb4e29b03837c05a
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-03-09 11:45:59 +0800

    feat: add Project class to store tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import './style.css';
 
+class Project {
+  constructor(name) {
+    this.name = name;
+  }
+
+  #tasks = [];
+
+  addTask(task) {
+    this.#tasks.push(task);
+  }
+
+  displayAllTasks() {
+    this.#tasks.forEach(task => {
+      console.log(task);
+    });
+  }
+}
+
 class Task {
   constructor(title, desc, dueDate, priority) {
     this.title = title;
@@ -8,13 +26,27 @@ class Task {
     this.priority = priority;
   }
 
+  setTitle(newTitle) { this.title = newTitle; }
+  setDesc(newDesc) { this.desc = newDesc; }
+  setDueDate(newDueDate) { this.dueDate = newDueDate; }
+  setPriority(newPriority) { this.priority = newPriority; }
+
   displayTask() {
     console.log(`${this.title} ${this.desc} ${this.dueDate} ${this.priority}`);
   }
 }
 
+const project1 = new Project('morning routine');
+const project2 = new Project('afternoon routine');
+
 const task1 = new Task('shower', 'first desc', 'today', 'high');
 const task2 = new Task('code', 'second desc', 'tomorrow', 'medium');
+const task3 = new Task('eat', 'third desc', 'later', 'low');
 
-task1.displayTask();
-task2.displayTask();
+project1.addTask(task1);
+project1.addTask(task2);
+
+project2.addTask(task3);
+
+project1.displayAllTasks();
+project2.displayAllTasks();
